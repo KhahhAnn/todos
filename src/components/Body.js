86c9f03aa106e519960f2efdf7f2729ff0fbe57d
khahhan <khanhanbui1010@gmail.com>
2023-11-13 13:52:00 +0700

    update

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
+import React, { useState, useEffect } from "react";
 import ToDoItem from "./ToDoItem";
 
 const Body = ({ toDoItems, onDelete, onToggle, onEdit }) => {
@@ -7,6 +6,7 @@ const Body = ({ toDoItems, onDelete, onToggle, onEdit }) => {
    const [editableId, setEditableId] = useState(null);
    const [editText, setEditText] = useState("");
    const [itemsToShow, setItemsToShow] = useState(5);
+   const [isLoading, setIsLoading] = useState(false);
 
    const handleDelete = (id) => {
       onDelete(id);
@@ -28,27 +28,31 @@ const Body = ({ toDoItems, onDelete, onToggle, onEdit }) => {
    };
 
    const handleScroll = (event) => {
-      const element = event.target;                    
+      const element = event.target;
       if (element.scrollHeight - element.scrollTop === element.clientHeight) {
          fetchMoreData();
       }
    };
 
    const fetchMoreData = () => {
+      setIsLoading(true);
       setTimeout(() => {
          setItemsToShow((prevItems) => prevItems + 5);
-      }, 1000); 
+         setIsLoading(false);
+      }, 1000);
    };
 
    useEffect(() => {
       setItemsToShow(5);
    }, [toDoItems]);
+
    return (
       <div
          className="todo-container"
          id="scrollableDiv"
          style={{
             overflowY: toDoItems.length > 5 ? "scroll" : "auto",
+            maxHeight: "300px", 
          }}
          onScroll={handleScroll}
       >
@@ -71,6 +75,7 @@ const Body = ({ toDoItems, onDelete, onToggle, onEdit }) => {
                handleDelete={handleDelete}
             />
          ))}
+         {isLoading && <p>Loading...</p>}
       </div>
    );
 };
